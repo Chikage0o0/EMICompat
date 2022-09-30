6eaf2d4869e13ffc6617d050bf05ebbe15959245
Chikage <chikage@939.me>
2022-09-30 15:49:05 +0800

    complate CraftingTerm support

--- src/main/java/me/chikage/emicompat/ae2/transfer/UseCraftingRecipeTransfer.java
+++ src/main/java/me/chikage/emicompat/ae2/transfer/UseCraftingRecipeTransfer.java
@@ -1,24 +1,38 @@
 package me.chikage.emicompat.ae2.transfer;
 
 
+import appeng.api.stacks.AEItemKey;
+import appeng.api.stacks.AEKey;
+import appeng.core.sync.network.NetworkHandler;
+import appeng.core.sync.packets.FillCraftingGridFromRecipePacket;
 import appeng.menu.SlotSemantics;
+import appeng.menu.me.common.GridInventoryEntry;
+import appeng.menu.me.common.MEStorageMenu;
 import appeng.menu.me.items.CraftingTermMenu;
+import com.google.common.base.Preconditions;
+import dev.emi.emi.api.EmiFillAction;
 import dev.emi.emi.api.EmiRecipeHandler;
+import dev.emi.emi.api.recipe.EmiCraftingRecipe;
+import dev.emi.emi.api.recipe.EmiPlayerInventory;
 import dev.emi.emi.api.recipe.EmiRecipe;
 import dev.emi.emi.api.recipe.VanillaEmiRecipeCategories;
+import dev.emi.emi.api.stack.EmiIngredient;
+import dev.emi.emi.api.stack.EmiStack;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.core.NonNullList;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
-import org.jetbrains.annotations.Nullable;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.ShapedRecipe;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 
 public class UseCraftingRecipeTransfer implements EmiRecipeHandler<CraftingTermMenu> {
-    public UseCraftingRecipeTransfer() {
-    }
+    private final Comparator<GridInventoryEntry> ENTRY_COMPARATOR = Comparator
+            .comparing(GridInventoryEntry::getStoredAmount);
 
     @Override
     public List<Slot> getInputSources(CraftingTermMenu handler) {
@@ -33,14 +47,15 @@ public class UseCraftingRecipeTransfer implements EmiRecipeHandler<CraftingTermM
         return list;
     }
 
+
     @Override
-    public @Nullable Slot getOutputSlot(CraftingTermMenu handler) {
-        return handler.getSlots(SlotSemantics.CRAFTING_RESULT).get(0);
+    public boolean canCraft(EmiRecipe recipe, EmiPlayerInventory inventory, AbstractContainerScreen<CraftingTermMenu> screen) {
+        return true;
     }
 
     @Override
     public List<Slot> getCraftingSlots(CraftingTermMenu handler) {
-        return handler.getSlots(SlotSemantics.CRAFTING_GRID);
+        return Collections.emptyList();
     }
 
     @Override
@@ -48,4 +63,100 @@ public class UseCraftingRecipeTransfer implements EmiRecipeHandler<CraftingTermM
         return recipe.getCategory() == VanillaEmiRecipeCategories.CRAFTING && recipe.supportsRecipeTree();
     }
 
+    @Override
+    public boolean performFill(EmiRecipe recipe, AbstractContainerScreen<CraftingTermMenu> screen, EmiFillAction action, int amount) {
+        if (recipe instanceof EmiCraftingRecipe) {
+            var templateItems = findGoodTemplateItems(recipe, screen.getMenu());
+
+            var recipeId = recipe.getId();
+            Minecraft.getInstance().setScreen(screen);
+            NetworkHandler.instance().sendToServer(new FillCraftingGridFromRecipePacket(recipeId, templateItems, true));
+
+            return true;
+        }
+        return false;
+
+    }
+
+
+    protected final Map<AEKey, Integer> getIngredientPriorities(MEStorageMenu menu,
+                                                                Comparator<GridInventoryEntry> comparator) {
+        var orderedEntries = menu.getClientRepo().getAllEntries()
+                .stream()
+                .sorted(comparator)
+                .map(GridInventoryEntry::getWhat)
+                .toList();
+
+        var result = new HashMap<AEKey, Integer>(orderedEntries.size());
+        for (int i = 0; i < orderedEntries.size(); i++) {
+            result.put(orderedEntries.get(i), i);
+        }
+
+        // Also consider the player inventory, but only as the last resort
+        for (var item : menu.getPlayerInventory().items) {
+            var key = AEItemKey.of(item);
+            if (key != null) {
+                // Use -1 as lower priority than the lowest network entry (which start at 0)
+                result.putIfAbsent(key, -1);
+            }
+        }
+
+        return result;
+    }
+
+    private NonNullList<ItemStack> findGoodTemplateItems(EmiRecipe recipe, MEStorageMenu menu) {
+        var ingredientPriorities = getIngredientPriorities(menu, ENTRY_COMPARATOR);
+
+        var templateItems = NonNullList.withSize(9, ItemStack.EMPTY);
+        var ingredients = ensure3by3CraftingMatrix(recipe);
+        for (int i = 0; i < ingredients.size(); i++) {
+            var ingredient = ingredients.get(i);
+            if (!ingredient.isEmpty()) {
+                // Try to find the best item. In case the ingredient is a tag, it might contain versions the
+                // player doesn't actually have
+                var stack = ingredientPriorities.entrySet()
+                        .stream()
+                        .filter(e -> e.getKey() instanceof AEItemKey itemKey && EmiIngredient.areEqual(ingredient, EmiStack.of(itemKey.toStack())))
+                        .max(Comparator.comparingInt(Map.Entry::getValue))
+                        .map(e -> ((AEItemKey) e.getKey()).toStack())
+                        .orElse(ingredient.getEmiStacks().get(0).getItemStack());
+
+                templateItems.set(i, stack);
+            }
+        }
+        return templateItems;
+    }
+
+    private NonNullList<EmiIngredient> ensure3by3CraftingMatrix(EmiRecipe recipe) {
+        var ingredients = recipe.getInputs();
+        var expandedIngredients = NonNullList.withSize(9, EmiIngredient.of(Ingredient.EMPTY));
+
+
+        Preconditions.checkArgument(ingredients.size() <= 9);
+
+        // shaped recipes can be smaller than 3x3, expand to 3x3 to match the crafting
+        // matrix
+        if (recipe instanceof ShapedRecipe shapedRecipe) {
+            var width = shapedRecipe.getWidth();
+            var height = shapedRecipe.getHeight();
+            Preconditions.checkArgument(width <= 3 && height <= 3);
+
+            for (var h = 0; h < height; h++) {
+                for (var w = 0; w < width; w++) {
+                    var source = w + h * width;
+                    var target = w + h * 3;
+                    var i = ingredients.get(source);
+                    expandedIngredients.set(target, i);
+                }
+            }
+        }
+        // Anything else should be a flat list
+        else {
+            for (var i = 0; i < ingredients.size(); i++) {
+                expandedIngredients.set(i, ingredients.get(i));
+            }
+        }
+
+        return expandedIngredients;
+    }
 }
